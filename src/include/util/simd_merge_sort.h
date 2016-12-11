8eb3207a18004dd08ad6557cef550c166ad5ecc6
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-12-11 18:44:28 -0500

    Added streaming instructions

--- src/include/util/simd_merge_sort.h
+++ src/include/util/simd_merge_sort.h
@@ -24,11 +24,11 @@ namespace util {
 typedef long long sort_ele_type;
 
 inline __m256i load_reg256(sort_ele_type *a) {
-  return *(__m256i*)a;
+  return _mm256_stream_load_si256((__m256i*)a);
 }
 
 inline void store_reg256(sort_ele_type *a, __m256i& b) {
-  *((__m256i*)a) = b;
+  _mm256_stream_si256((__m256i*)a, b);
 }
 
 std::pair<sort_ele_type *, sort_ele_type *> simd_merge_sort(sort_ele_type *a,
